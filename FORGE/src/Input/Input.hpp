47bc6e50bc96dce524941221d1e711ebc74becd9
Alberto Peñalba Martos <albpenal@ucm.es>
2024-02-23 10:59:26 +0100

    Métodos iniciales input y testing

--- FORGE/src/Input/Input.hpp
+++ FORGE/src/Input/Input.hpp
@@ -1,4 +1,57 @@
+#include "SDL.h"
+
 
 class Input {
-	
+private:
+	SDL_Event SDLPreviousEvent;
+protected:
+
+public:
+	Input() {
+		SDLPreviousEvent.type = SDL_KEYUP;
+	}
+
+	char KeyDown(SDL_Event SDLevent) {
+		if (SDLPreviousEvent.type == SDL_KEYUP) {
+			if (SDLevent.type == SDL_KEYDOWN) {
+				std::cout << "pulsada\n";
+				SDLPreviousEvent.type = SDL_KEYDOWN;
+				return SDLevent.key.keysym.sym;
+			}
+		}
+		return ' ';
+	}
+
+	char KeyPressed(SDL_Event SDLevent) {
+		if (SDLPreviousEvent.type == SDL_KEYDOWN) {
+			if (SDLevent.type == SDL_KEYDOWN) {
+				std::cout << "mantenido\n";
+				SDLPreviousEvent.type = SDL_KEYDOWN;
+				return SDLevent.key.keysym.sym;
+			}
+		}
+		return ' ';
+	}
+
+	char KeyUp(SDL_Event SDLevent) {
+		if (SDLevent.type == SDL_KEYUP) {
+			std::cout << "soltada\n";
+			SDLPreviousEvent.type = SDL_KEYUP;
+			return SDLevent.key.keysym.sym;
+		}
+		return ' ';
+	}
+
+	//std::string KeyPressedS(SDL_Event SDLevent) {
+	//	if (SDLPreviousEvent.type == SDL_KEYDOWN) {
+	//		if (SDLevent.type == SDL_KEYDOWN) {
+	//			std::cout << "mantenido\n";
+	//			SDLPreviousEvent.type = SDL_KEYDOWN;
+	//			//std::cout << "KeyDown_" << (char)SDLevent.key.keysym.sym << "\n";
+	//			char c = SDLevent.key.keysym.sym;
+	//			return ("KeyDown_" + c);
+	//		}
+	//	}
+	//	return "";
+	//}
 };
--- FORGE/src/Input/InputTest.cpp
+++ FORGE/src/Input/InputTest.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include "SDL.h"
+#include "Input.hpp"
 
 int main() {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	char c = ' ';     
 	SDL_Init(SDL_INIT_EVERYTHING);
 	auto window = SDL_CreateWindow("Prueba", 100, 100, 900, 600, SDL_WINDOW_SHOWN);
-	while (c == ' ') {
-		SDL_Event SDLevent;         
+	Input* input = new Input();
+	SDL_Event SDLevent;
+	while (c == ' ') {      
 		while (SDL_PollEvent(&SDLevent)) {
-			if (SDLevent.type == SDL_KEYDOWN)
-				std::cout << "Hey :)\n";         
+			//if (SDLevent.type = SDL_CONTROLLERBUTTONDOWN) std::cout << "AAAAAAA\n";
+			//std::string st = input->KeyPressedS(SDLevent);
+			input->KeyPressed(SDLevent);
+			input->KeyUp(SDLevent);
+			input->KeyDown(SDLevent);
 		}        
 	}      
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+
 	return 0; 
 }
