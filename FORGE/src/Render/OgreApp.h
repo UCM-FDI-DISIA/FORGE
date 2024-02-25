16ce4170098a44aa92bc1f6218861bb058259a4d
Rafael Argandoña Blacido <rargan01@ucm.es>
2024-02-25 11:19:07 +0100

    Intento de abrir ventana

---
+++ FORGE/src/Render/OgreApp.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <OgreRoot.h>
+#include <SDL.h>
+#include <OgreFileSystemLayer.h>
+#include <OgreOverlaySystem.h>
+#include <OgreBuildSettings.h>
+#include <OgreLogManager.h>
+#include <OgrePlugin.h>
+#include <OgreFileSystemLayer.h>
+#include <OgreFrameListener.h>
+#include <OgreRTShaderConfig.h>
+#include <OgreRTShaderExports.h>
+
+namespace Render {
+	typedef SDL_Window NativeWindowType;
+
+	/**
+	link between a renderwindow and a platform specific window
+	*/
+	struct NativeWindowPair
+	{
+		Ogre::RenderWindow* render = nullptr;
+		NativeWindowType* native = nullptr;
+	};
+
+
+	class OgreApp : public Ogre::FrameListener {
+	protected:
+		Ogre::Root* mRoot;        // OGRE root
+		NativeWindowPair mWindow; // the window
+
+		Ogre::FileSystemLayer* mFSLayer; // File system abstraction layer
+		Ogre::OverlaySystem* mOverlaySystem;  // Overlay system
+		
+		bool mFirstRun;
+		Ogre::String mAppName;
+		Ogre::String mSolutionPath;    // IG2: variable para hacer las rutas relativas al directorio de la solución
+		
+		Ogre::String mRTShaderLibPath;
+		Ogre::SceneManager* mSM = nullptr;
+		// Ogre::RTShader::ShaderGenerator* mShaderGenerator; // The Shader generator instance.
+		// SGTechniqueResolverListener* mMaterialMgrListener; // Shader generator material manager listener.
+
+	public:
+		OgreApp();
+
+		bool go();
+		void createRoot();
+		void setup();
+
+		NativeWindowPair createWindow(Ogre::String name);
+		void setWindowGrab(bool active);   // IG2: ratón libre
+
+		void locateResources();
+		//bool initialiseRTShaderSystem();
+	};
+}
--- FORGE/src/Render/prueba.cpp
+++ FORGE/src/Render/prueba.cpp
@@ -1,77 +1,9 @@
 // Este archivo es para que se suba la carpeta src/Render a Github
-#include <OgreException.h>
-#include <OgreRoot.h>
-#include <OgreConfigFile.h>
-#include "OgreTextureManager.h"
-#include "OgreRenderWindow.h"
-class TutorialApplication
-{
-    private:
-        Ogre::Root* mRoot;
-        Ogre::String mResourcesCfg;
-        Ogre::String mPluginsCfg;
-    public:
-    TutorialApplication(): mRoot(0), mResourcesCfg(Ogre::BLANKSTRING),mPluginsCfg(Ogre::BLANKSTRING)
-    {
-    }
-    
-    virtual ~TutorialApplication()
-    {
-        delete mRoot;
-    }
-    
-    bool go()
-    {
-#ifdef _DEBUG
-        mResourcesCfg = "resources_d.cfg";
-        mPluginsCfg = "plugins_d.cfg";
-#else
-        mResourcesCfg = "resources.cfg";
-        mPluginsCfg = "plugins.cfg";
-#endif
 
-        mRoot = new Ogre::Root(mPluginsCfg);
+#include "OgreApp.h"
 
-        Ogre::ConfigFile cf;
-        cf.load(mResourcesCfg);
-
-        Ogre::String name, locType;
-        auto secIt = cf.getSettingsBySection().begin();
-
-        while (secIt!= cf.getSettingsBySection().end())
-        {
-            auto settings = (*secIt).second;
-            Ogre::ConfigFile::SettingsMultiMap::iterator it;
-
-            for (it = settings.begin(); it != settings.end(); ++it)
-            {
-                locType = it->first;
-                name = it->second;
-
-                Ogre::ResourceGroupManager::getSingleton().addResourceLocation(name, locType);
-            }
-            secIt++;
-        }
-
-        Ogre::RenderWindow* mWindow;
-        if (!(mRoot->restoreConfig() || mRoot->showConfigDialog(nullptr))) return false;
-
-        mWindow = mRoot->initialise(true, "TutorialApplication Render Window");
-        Ogre::TextureManager::getSingleton().setDefaultNumMipmaps(5);
-        Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
-
-        return true;
-    }
-   
-    
-};
-
-
-
-
-int main()
-{
-    TutorialApplication hola;
+int main(int argc, char* argv[]) {
+    Render::OgreApp hola;
     hola.go();
     return 0;
 }
