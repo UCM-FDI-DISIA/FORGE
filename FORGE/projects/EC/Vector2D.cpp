49a8c0beb1dab20ed7e0e6c72505b1dbf9a49d52
sosanc03 <99793450+sosanc03@users.noreply.github.com>
2024-03-01 10:14:02 +0100

    Vector2D

---
+++ FORGE/projects/EC/Vector2D.cpp
@@ -0,0 +1,58 @@
+#include "Vector2D.h"
+
+Vector2D::Vector2D() :X(), Y() { }
+Vector2D::Vector2D( float x = 0, float y = 0 ):X(x), Y(y) { }
+Vector2D::Vector2D(const Vector2D& v) : X(v.X), Y(v.Y) { }
+Vector2D::~Vector2D(){}
+
+Vector2D Vector2D::operator+( const Vector2D& v ) const {
+	return Vector2D( X + v.X, Y + v.Y );
+}
+Vector2D Vector2D::operator-( const Vector2D& v ) const {
+	return Vector2D( X - v.X, Y - v.Y );
+}
+Vector2D Vector2D::operator*( float e ) const {
+	return Vector2D( X * e, Y * e );
+}
+Vector2D Vector2D::operator/( float e ) const {
+	return Vector2D( X / e, Y / e );
+}
+float Vector2D::operator *( const Vector2D& v ) const {
+	return ( X * v.X + Y * v.Y );
+}
+bool Vector2D::operator==( const Vector2D& v ) const {
+	return (X == v.X && Y == v.Y);
+}
+bool Vector2D::operator!=( const Vector2D& v ) const {
+	return (X != v.X || Y != v.Y);
+}
+
+float Vector2D::magnitude() const {
+	return sqrtf( X*X + Y*Y );
+}
+Vector2D Vector2D::normalize() const {
+	return *this / magnitude();
+}
+
+float Vector2D::getX() const{
+	return X;
+}
+
+float Vector2D::getY() const{
+	return Y;
+}
+
+void Vector2D::setX(float x) {
+	X = x;
+}
+void Vector2D::setY(float y) {
+	Y = y;
+}
+void Vector2D::set( const Vector2D& v ) {
+	X = v.X;
+	Y = v.Y;
+}
+void Vector2D::set(float x, float y) {
+	X = x;
+	Y = y;
+}
---
+++ FORGE/projects/EC/Vector2D.h
@@ -0,0 +1,95 @@
+#pragma once
+#ifndef VECTOR2D_H_
+#define VECTOR2D_H_
+#include <cmath>
+class Vector2D
+{
+private:
+	// Primera coordenada
+	float X;
+	// Segunda coordenada
+	float Y;
+public:
+	/// <summary>
+	/// Constructora por defecto de la clase Vector2D
+	/// </summary>	
+	Vector2D();
+	/// <summary>
+	/// Constructora a partir de parametros
+	/// </summary>	
+	Vector2D(float x = 0, float y = 0);
+	/// <summary>
+	/// Constructora a partir de otro vector
+	/// </summary>	
+	Vector2D(const Vector2D& v);
+	/// <summary>
+	/// Destructora por defecto de la clase Vector2D
+	/// </summary>
+	~Vector2D();
+
+	/// <summary>
+	/// Suma dos vectores
+	/// </summary>	
+	Vector2D operator+( const Vector2D& v ) const;
+	/// <summary>
+	/// Resta dos vectores
+	/// </summary>	
+	Vector2D operator-( const Vector2D& v ) const;
+	/// <summary>
+	/// Multiplica un vector por un escalar
+	/// </summary>	
+	Vector2D operator*( float e ) const;
+	/// <summary>
+	/// Divide un vector por un escalar
+	/// </summary>
+	Vector2D operator/( float e ) const;
+	/// <summary>
+	/// Producto escalar
+	/// </summary>
+	float operator *( const Vector2D& v ) const;
+	/// <summary>
+	/// Igualdad de dos vectores
+	/// </summary>
+	bool operator ==( const Vector2D& v ) const;
+	/// <summary>
+	/// Desigualdad de dos vectores
+	/// </summary>
+	bool operator !=( const Vector2D& v ) const;
+
+	/// <summary>
+	/// Calcula el modulo de un vector
+	/// </summary>
+	float magnitude() const;
+	/// <summary>
+	/// Normaliza un vector
+	/// </summary>
+	Vector2D normalize() const;
+
+	/// <summary>
+	/// Devuelve el valor de la coordenada X
+	/// </summary>	
+	float getX() const;
+	/// <summary>
+	/// Devuelve el valor de la coordenada y
+	/// </summary>	
+	float getY() const;
+
+	/// <summary>
+	/// Setea el valor de la coordenada X
+	/// </summary>	
+	void setX(float x);
+	/// <summary>
+	/// Setea el valor de la coordenada y
+	/// </summary>	
+	void setY(float y);
+	/// <summary>
+	/// Setea un vector a través de otro vector
+	/// </summary>	
+	void set(const Vector2D& v);
+	/// <summary>
+	/// Setea un vector a través de floats
+	/// </summary>	
+	void set(float x, float y);
+};
+#endif
+
