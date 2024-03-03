629a39e7f1eda53bfac32b9e8159e014147a968b
Rafael Argandoña Blacido <rargan01@ucm.es>
2024-03-03 16:51:08 +0100

    Vector3, Vector2, Quaternion
    Implementaciones iniciales

---
+++ FORGE/src/Utils/Quaternion.cpp
@@ -0,0 +1,105 @@
+#include "Quaternion.h"
+
+#define kEpsilon = 0.000001
+
+Quaternion::Quaternion() : x(0), y(0), z(0) {}
+
+Quaternion::Quaternion(float _x, float _y, float _z, float _w) {
+	set(_x, _y, _z, _w);
+}
+
+Quaternion::Quaternion(const Quaternion& v) : x(v.x), y(v.y), z(v.z) {}
+
+Quaternion::Quaternion(Quaternion&& v) noexcept :
+	x(v.x), y(v.y), z(v.z) {}
+
+Quaternion::Quaternion(const Quaternion* v) :
+	x(v->x), y(v->y), z(v->z) {}
+
+Quaternion::~Quaternion() {}
+
+#pragma region Operadores
+Quaternion& Quaternion::operator=(const Quaternion& q) {
+
+}
+
+float Quaternion::dot(const Quaternion& q) const{
+	return x * q.x + y * q.y + z * q.z + w * q.w;
+}
+
+Quaternion Quaternion::operator*(const Quaternion& q) const {
+	return Quaternion(w * q.x + x * q.w + y * q.z - z * q.y, 
+		w * q.y + y * q.w +	z * q.x - x * q.z, 
+		w * q.z + z * q.w + x * q.y - y * q.x,
+		w * q.w - x * q.x - y * q.y - z * q.z);
+}
+
+bool Quaternion::operator==(const Quaternion& q) const{
+	return dot(q) > 0.999999f;
+}
+
+bool Quaternion::operator!=(const Quaternion& q) const{
+	return dot(q) <= 0.999999f;
+}
+
+Vector3 Quaternion::operator*(const Vector3& v) const {
+	float num = x * 2.0f;
+	float num2 = y * 2.0f;
+	float num3 = z * 2.0f;
+	float num4 = x * num;
+	float num5 = y * num2;
+	float num6 = z * num3;
+	float num7 = x * num2;
+	float num8 = x * num3;
+	float num9 = y * num3;
+	float num10 = w * num;
+	float num11 = w * num2;
+	float num12 = w * num3;
+	Vector3 result = Vector3();
+	float rX = (1.0f - (num5 + num6)) * v.getX() + (num7 - num12) * v.getY() + (num8 + num11) * v.getZ();
+	float rY = (num7 + num12) * v.getX() + (1.0f - (num4 + num6)) * v.getY() + (num9 - num10) * v.getZ();
+	float rZ = (num8 - num11) * v.getX() + (num9 + num10) * v.getY() + (1.0f - (num4 + num5)) * v.getZ();
+	result.set(rX, rY, rZ);
+	return result;
+}
+#pragma endregion
+
+
+#pragma region Setters
+void Quaternion::setX(float _x) { x = _x; }
+void Quaternion::setY(float _y) { y = _y; }
+void Quaternion::setZ(float _z) { z = _z; }
+void Quaternion::setW(float _w) { w = _w; }
+
+void Quaternion::set(float _x, float _y, float _z, float _w) {
+	x = _x; y = _y; z = _z; w = _w;
+}
+
+void Quaternion::set(const Quaternion& v) {
+	x = v.x;
+	y = v.y;
+	z = v.z;
+	w = v.w;
+}
+
+void Quaternion::set(const Quaternion&& v) {
+	x = v.x;
+	y = v.y;
+	z = v.z;
+	w = v.w;
+}
+
+void Quaternion::set(const Quaternion* v) {
+	x = v->x;
+	y = v->y;
+	z = v->z;
+	w = v->w;
+}
+#pragma endregion
+
+#pragma region Getters
+float Quaternion::getX() const { return x; }
+float Quaternion::getY() const { return y; }
+float Quaternion::getZ() const { return z; }
+float Quaternion::getW() const { return w; }
+#pragma endregion
---
+++ FORGE/src/Utils/Vector3.cpp
@@ -0,0 +1,156 @@
+#include "Vector3.h"
+
+#define PI 3.14159265358979323846264338327950288f
+#define E 2.71828182845904523536
+
+Vector3::Vector3() : x(0), y(0), z(0) { } 
+
+Vector3::Vector3(float _x, float _y, float _z) {
+	set(_x, _y, _z);
+}
+
+Vector3::Vector3(const Vector3& v) : x(v.x), y(v.y), z(v.z) { }
+
+Vector3::Vector3(Vector3&& v) noexcept :
+	x(v.x), y(v.y), z(v.z) { }
+
+Vector3::Vector3(const Vector3* v) :
+	x(v->x), y(v->y), z(v->z) {	}
+
+Vector3::~Vector3() {}
+
+float Vector3::magnitude() const {
+	return sqrt(x * x + y * y + z * z);
+}
+
+float Vector3::sqrMagnitude() const {
+	return magnitude() * magnitude();
+}
+
+Vector3 Vector3::normalised() const{
+	float m = magnitude();
+	return Vector3(x / m, y / m, z / m);
+}
+
+
+
+float Vector3::distance(const Vector3& v) const {
+	float m = Vector3(x - v.x, y - v.y, z - v.z).magnitude();
+	return m;
+}
+
+Vector3 Vector3::maxV(const Vector3& v) const {
+	return Vector3(std::fmaxf(x, v.x), std::fmaxf(y, v.y), std::fmaxf(z, v.z));
+}
+
+Vector3 Vector3::minV(const Vector3& v) const {
+	return Vector3(std::fminf(x, v.x), std::fminf(y, v.y), std::fminf(z, v.z));
+}
+
+Vector3 Vector3::Lerp(const Vector3& v, float t) const {
+	return Vector3(x + (v.x - x) * t, y + (v.y - y) * t, z + (v.z - z) * t);
+}
+
+#pragma region Operadores
+Vector3& Vector3::operator=(const Vector3& v) {
+	x = v.x;
+	y = v.y;
+	z = v.z;
+	return *this;
+}
+
+Vector3 Vector3::operator-(const Vector3& v) const {
+	return Vector3(x - v.x, y - v.y, z - v.z);
+}
+
+Vector3 Vector3::operator+(const Vector3& v)  const {
+	return Vector3(x + v.x, y + v.y, z + v.z);
+}
+
+Vector3 Vector3::operator*(float d)  const {
+	return Vector3(x * d, y * d, z * d);
+}
+
+Vector3 Vector3::operator/(float d)  const {
+	return Vector3(x / d, y / d, z / d);
+}
+
+float Vector3::operator*(const Vector3& d)  const {
+	return d.x * x + d.y * y + d.z * z;
+}
+
+bool Vector3::operator==(const Vector3& v) const {
+	float m1 = magnitude();
+	float m2 = v.magnitude();
+	float min = pow(1, E - 5);
+	return (abs(m1 - m2) <= min);
+}
+
+bool Vector3::operator!=(const Vector3& v)  const {
+	Vector3 diff = Vector3(x, y, z) - v;
+	float min = 1 * pow(10, -5);
+	return (abs(diff.magnitude()) <= min);
+}
+#pragma endregion
+
+
+float Vector3::angle(const Vector3& v) const {
+	float prodEsc = Vector3(x,y,z) * v;
+	float angle = std::acos(prodEsc / (magnitude() * v.magnitude()));
+
+	return angle * 180 / PI;
+}
+
+Vector3 Vector3::reflect(const Vector3& normal) const {
+	Vector3 nNormalised = normal.normalised();
+	float dot = Vector3(x, y, z) * nNormalised;
+	Vector3 reflectPart = nNormalised * (2 * dot);
+	return Vector3(x,y,z) - reflectPart;
+}
+
+Vector3 Vector3::project(const Vector3& v) {
+	float dot = Vector3(x, y, z) * v;
+	float div = dot / v.sqrMagnitude();
+	return v * div;
+}
+
+Vector3 Vector3::cross(const Vector3& v) const {
+	float compX = y * v.z - z * v.y;
+	float compY = x * v.z - z * v.x;
+	float compZ = x * v.y - y * v.x;
+	return Vector3(compX, -1.0f * compY, compZ);
+}
+
+#pragma region Setters
+void Vector3::setX(float _x) { x = _x; }
+void Vector3::setY(float _y) { y = _y; }
+void Vector3::setZ(float _z) { z = _z; }
+
+void Vector3::set(float _x, float _y, float _z) {
+	x = _x; y = _y; z = _z;
+}
+
+void Vector3::set(const Vector3& v) {
+	x = v.x;
+	y = v.y;
+	z = v.z;
+}
+
+void Vector3::set(const Vector3&& v) {
+	x = v.x;
+	y = v.y;
+	z = v.z;
+}
+
+void Vector3::set(const Vector3* v) {
+	x = v->x;
+	y = v->y;
+	z = v->z;
+}
+#pragma endregion
+
+#pragma region Getters
+float Vector3::getX() const { return x; }
+float Vector3::getY() const { return y; }
+float Vector3::getZ() const { return z; }
+#pragma endregion
