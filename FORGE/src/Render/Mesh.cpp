e4532fab7a009d007806858c8bf4530b24a6b4bd
algonz39 <algonz39@ucm.es>
2024-03-14 12:47:31 +0100

    Cosas para crear el componente Animation

--- FORGE/src/Render/Mesh.cpp
+++ FORGE/src/Render/Mesh.cpp
@@ -4,15 +4,18 @@
 #include "Serializer.h"
 #include "OgreEntity.h"
 
-const std::string Mesh::id = "Mesh";
+const std::string RenderData::id = "Mesh";
 
-Mesh::Mesh() : 
+Mesh::Mesh() :
 	mesh(),
 	material(),
+    activeAnimation(),
     ogreEntity(nullptr),
+    animations(nullptr),
     renderManager(nullptr) {
 	serializer(mesh, "mesh");
 	serializer(material, "material");
+	serializer(activeAnimation, "activeAnimation");
 }
 
 Mesh::~Mesh() {
@@ -23,16 +26,20 @@ void Mesh::initComponent(ComponentData* data) {
     if(entity->hasComponent("Transform")) {
         renderManager = RenderManager::getInstance();
         ogreEntity = renderManager->addMeshNode(this);
+        animations = ogreEntity->getAllAnimationStates();
     }
 }
 
 void Mesh::setEnabled(bool newActive) {
     Component::setEnabled(newActive);
     if (newActive) {
-        renderManager->addMeshNode(this);
+        ogreEntity = renderManager->addMeshNode(this);
+        animations = ogreEntity->getAllAnimationStates();
     }
     else {
         renderManager->removeNode(ogreEntity);
+        ogreEntity = nullptr;
+        animations = nullptr;
     }
 }
 
@@ -45,6 +52,13 @@ void Mesh::setMaterial(std::string newMaterial) {
     material = newMaterial;
     ogreEntity->setMaterialName(newMaterial);
 }
+
+
+void Mesh::setAnimation(std::string newMaterial) {
+    activeAnimation = animations;
+    ogreEntity->setMaterialName(newMaterial);
+}
+
 
 const std::string& Mesh::getMesh() const {
     return mesh;
