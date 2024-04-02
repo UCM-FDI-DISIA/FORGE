5375630a630bfd0d0e938aee6779d5ca1c6fa590
Rafael Argandoña Blacido <rargan01@ucm.es>
2024-04-02 21:52:15 +0200

    Cambio nombre de variable
    Antes resultaba poco intuitivo

--- FORGE/src/Load/LuaForge.cpp
+++ FORGE/src/Load/LuaForge.cpp
@@ -44,11 +44,11 @@ lua_State* LuaForge::getState() const {
 }
 
 bool LuaForge::doFile(std::string path) {
-	bool fileFound = luaL_dofile(lua, path.c_str());
-	if (fileFound) {
+	bool fileNotFound = luaL_dofile(lua, path.c_str());
+	if (fileNotFound) {
 		std::cerr << "ERROR: Archivo .lua no encontrado o contiene un error de sintaxis\n";
 	}
-	return fileFound;
+	return fileNotFound;
 }
 
 void LuaForge::importClassToLua(std::function<void(lua_State*)> classCreation) {
